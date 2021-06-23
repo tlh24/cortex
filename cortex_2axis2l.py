b51eaeaa4de69a2c78a27f937eef8987448e34c6
Tim Hanson <sideskate@gmail.com>
2021-06-23 18:47:29 -0400

    multiple layers are still not working.  Feel i need a conceptual leap here!

--- cortex_2axis2l.py
+++ cortex_2axis2l.py
@@ -85,53 +85,102 @@ if False:
 
 torch.set_printoptions(sci_mode=False, linewidth=140)
 err = 0.0
-for k in range(10): 
-	w_f = torch.mul(torch.rand(2, 6), math.sqrt(2.0 / 6.0))
-	w_b = torch.zeros(6, 3) # let hebb fill these in. 
-	w_l2i = torch.zeros(2, 2)
-	l2a = torch.ones(2) * 0.5
-	l2u5 = torch.ones(3)
-	for i in range(10000): # realistically, need far fewer than 10k...
+N = 100
+for k in range(1): 
+	w_f2 = torch.mul(torch.rand(4, 6), math.sqrt(2.0 / 6.0))
+	w_b2 = torch.zeros(6, 5) # let hebb fill these in. 
+	w_l2i = torch.zeros(4, 4)
+	l2a = torch.ones(4) * 0.5
+	l2u5 = torch.ones(5)
+	
+	w_f3 = torch.mul(torch.rand(2, 4), math.sqrt(2.0 / 6.0))
+	w_b3 = torch.zeros(4, 3)
+	w_l3i = torch.zeros(2,2)
+	l3a = torch.ones(2) * 0.5
+	l3u5 = torch.ones(3)
+	
+	for i in range(N): # realistically, need far fewer than 10k...
 		j = i % 4
 		ind = indata[j]; 
 		l1e_ = [ind[0], ind[0]^1, ind[1], ind[1]^1, ind[2], ind[2]^1]
 		l1e = torch.tensor(l1e_).to(torch.float)
+		l1u = l1e * 0.5
+		l2u = torch.zeros(4)
+		l2i = torch.zeros(4)
 
-		l2e = torch.clamp(torch.matmul(w_f, l1e), 0.0, 1.0)
-		l2i = torch.clamp(torch.matmul(w_l2i, l2e), 0.0, 2.0)
-		l2u = l2e - l2i
-		l2u5[0:2] = l2u + (torch.randn(2) * noise_level)
-		l2a = l2a * 0.99 + l2u * 0.01
-		
-		l1i = torch.clamp(torch.matmul(w_b, l2u5), 0.0, 1.0)
-		l1u = l1e - l1i + (torch.randn(6) * noise_level)
+		for k in range(5):
+			l2e = torch.clamp(torch.matmul(w_f2, l1e), 0.0, 1.0)
+				# propagate the signal
+			l2li = torch.clamp(torch.matmul(w_l2i, l2u), 0.0, 2.0)
+			l2u = l2e - l2li + l2i # NB! backward excitation!
+			l2u = l2u + torch.randn(4) * noise_level
+			l2a = l2a * 0.99 + l2u * 0.01
+			
+			l3e = torch.clamp(torch.matmul(w_f3, l2u), 0.0, 1.5)
+			l3li = torch.clamp(torch.matmul(w_l3i, l3e), 0.0, 3.0) # more async
+			l3u = l3e - l3li + (torch.randn(2) * noise_level)
+			l3a = l3a * 0.99 + l3u * 0.01
+			
+			l3u5[0:2] = l3u # include a bias term
+			l2i = torch.clamp(torch.matmul(w_b3, l3u5), 0.0, 1.0)
+			l2u = l2e - l2li + l2i # NB! backward excitation!
+			l2u = l2u + torch.randn(4) * noise_level
+			l2a = l2a * 0.99 + l2u * 0.01
+			
+			l2u5[0:4] = l2u
+			l1i = torch.clamp(torch.matmul(w_b2, l2u5), 0.0, 1.0)
+			l1u = l1e - l1i + (torch.randn(6) * noise_level)
+			
+			# propagate the error here; this is based on the annealed actvations.
+			l2r = torch.clamp(torch.matmul(w_f2, l1u), -1.0, 1.0)
+			
+			if i > N-5 and k >= 4:
+				print(i,k)
+				print('l1e', l1e[0:8])
+				print('l1i', l1i[0:8])
+				print('l1u', l1u[0:8])
+				print('l2e', l2e[0:8])
+				print('l2i', l2i[0:8])
+				print('l2li', l2li[0:8])
+				print('l2u', l2u[0:8])
+				print('l2r', l2r[0:8])
+				print('l2a', l2a[0:8])
+				print('l3e', l3e[0:8])
+				print('l3li', l3li[0:8])
+				print('l3a', l3a[0:8])
+				print('')
 		
 		# update forward weight based on inhibited l1 state. 
 		# e.g. at equilibrium, will be zero. 
-		w_f = hebb_update(w_f, l1u, l2u, l2a, 0.01)
-		w_l2i = inhib_update(w_l2i, l2u, l2i, 0.02)
-
-		w_b = hebb_update(w_b, l2u5, l1u, torch.ones(l1u.size(0)), 0.02)
+		w_f2 = hebb_update(w_f2, l1u, l2u, l2a, 0.001)
+		w_l2i = inhib_update(w_l2i, l2u, l2i, 0.002)
+		w_b2 = hebb_update(w_b2, l2u5, l1u, torch.ones(l1u.size(0)), 0.002)
+		
+		w_f3 = hebb_update(w_f3, l2r, l3u, l3a, 0.001)
+		w_l3i = inhib_update(w_l3i, l3u, l3li, 0.002)
+		w_b3 = hebb_update(w_b3, l3u5, l2r, torch.ones(l2u.size(0)), 0.002)
 		
 		er = torch.mean(torch.abs(l1u))
 		err = 0.99 * err + 0.01 * er
 		
-		if False: 
+		if True: 
 			print("i: ", i, "error: ", err)
 			#print("l1e", l1e)
 			#print("l1i", l1i)
 			#print("l1u", l1u)
 			#print("l2u", l2u)
-			print("l2a", l2a)
+			print("l3e", l3e)
 	
 	print("error:", err)
 	
-	if False:
+	if True:
 		print("l1e", l1e)
 		print("l1i", l1i)
 		print("l1u", l1u)
 		print("l2u", l2u)
 		print("l2a", l2a)
-		print("w_f", w_f)
-		print("w_b", w_b)
+		print("w_f2", w_f2)
+		print("w_b2", w_b2)
+		print("w_f3", w_f3)
+		print("w_b3", w_b3)
 		print(" ")
