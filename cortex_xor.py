d0b8aaa664b007f00e14706790320e20ec5a0c9e
Tim Hanson <sideskate@gmail.com>
2022-01-13 16:37:41 -0500

    refined the XOR task, removed a unecessary scaling rule, converges quicker now

--- cortex_xor.py
+++ cortex_xor.py
@@ -16,18 +16,18 @@ def inhib_update(w_, l, li, lr):
 	# this function also scales the weight matrix exponetially
 	# if any of the inputs are > 2.0. 
 	# inputs: w_, inhibitory weights
-	# l, the current layer e+i activation. 
+	# l, the current layer e+i activation aka u. 
 	# li, layer i (for scaling)
 	# lr, learning rate. 
-	#pdb.set_trace()
 	dw = torch.outer(l, l) # this doesn't break symmetry
 	dw2 = torch.outer(torch.pow(l, 2.0), torch.ones(l.size(0))) # this does!
 	# it also forces the diagonal to zero. 
 	dw2 = dw - dw2
 	dw2 = torch.clamp(dw2, -0.01, 0.1)
-	# dw2 = torch.clamp(dw - dw2, 0.0, 1.0); # only positive weight updates
 	dw2 = torch.mul(dw2, lr)
 	w_ = torch.add(w_, dw2)
+	# checked: this is needed for sparsity. 
+	# surprisingly, adding weight down-scaling makes them *larger* - ? 
 	scale = torch.clamp(li, 2.5, 1e6)
 	scale = torch.sub(scale, 2.5)
 	scale = torch.exp(torch.mul(scale, -0.04))
@@ -37,7 +37,6 @@ def inhib_update(w_, l, li, lr):
 
 
 def hebb_update(w_, inp, outp, outpavg, lr):
-# 	pdb.set_trace()
 	dw = torch.outer(outp, inp) 
 	# note: dw needs to be both positive and negative.  
 	# I tried clamping to [0 1] so as to feed into a power nonlinearity, 
@@ -52,21 +51,22 @@ def hebb_update(w_, inp, outp, outpavg, lr):
 	scale = torch.clamp(outp, 1.5, 1e6)
 	scale = torch.sub(scale, 1.5)
 	scale = torch.exp(torch.mul(scale, -0.04)) 
-	scale2 = torch.clamp(outpavg, 0.0, 0.06)
-	scale2 = torch.sub(0.06, scale2)
-	scale2 = torch.exp(torch.mul(scale2, 0.2))
-	scale = scale * scale2 
+	#scale2 = torch.clamp(outpavg, 0.0, 0.06) # this is unnecessary for convergence. 
+	#scale2 = torch.sub(0.06, scale2) # if outpavg is zero, this is 0.06
+	#scale2 = torch.exp(torch.mul(scale2, 0.2)) # then e^0.012 ~= 1.012
+	#scale = scale * scale2 # so if the output is too low, scale it up. 
 	dw = torch.outer(scale, torch.ones(inp.size(0)))
 	return torch.mul(w_, dw)
 
 w_f = torch.mul(torch.rand(4, 6), math.sqrt(2.0 / 6.0))
 w_b = torch.zeros(6, 4) # let hebb fill these in. 
 w_l2i = torch.zeros(4, 4)
+l1a = torch.zeros(6)
 l2a = torch.zeros(4)
 
 indata = [[0,0], [0,1], [1,0], [1,1]]
 
-for i in range(10000): # realistically, need far fewer than 10k...
+for i in range(1000): # realistically, need far fewer than 10k...
 	j = i % 4
 	ind = indata[j]; 
 	l1e_ = [ind[0], ind[0]^1, ind[1], ind[1]^1, ind[0]^ind[1], (ind[0]^ind[1])^1]
@@ -79,6 +79,7 @@ for i in range(10000): # realistically, need far fewer than 10k...
 	
 	l1i = torch.clamp(torch.matmul(w_b, l2u), 0.0, 2.5)
 	l1u = l1e - l1i
+	l1a = l1a * 0.99 + l1u * 0.01
 	
 	# update forward weight based on inhibited l1 state. 
 	# e.g. at equilibrium, will be zero. 
@@ -89,15 +90,18 @@ for i in range(10000): # realistically, need far fewer than 10k...
 	# humm .. ? 
 	# basically, the hebbian update (+) exactly counteracts the inhibition (-), and they reach equilibrium. Maybe just allow the inhibition to get larger ?! 
 	# similarly, update reverse weight .. 
-	w_b = hebb_update(w_b, l2u, l1u, torch.ones(l1u.size(0)), 0.02)
+	w_b = hebb_update(w_b, l2u, l1u, l1a, 0.02)
 	
 	if (i % 97) == 0: 
 		print("l1e", l1e)
 		print("l1i", l1i)
+		print("l1a", l1a)
 		print("l2u", l2u)
 		print("l2i", l2i)
 		print("  ")
 	
 	
-	
+#im = plt.imshow(w_l2i.numpy())
+#plt.colorbar(im)
+#plt.show()
 	
