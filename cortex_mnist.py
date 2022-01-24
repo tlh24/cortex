35acd730c2a28947f9d32bd770afa85c8cae5a3d
Tim Hanson <sideskate@gmail.com>
2022-01-24 18:27:14 -0500

    still working on the inhibition / sparsity metric..

--- cortex_mnist.py
+++ cortex_mnist.py
@@ -22,8 +22,8 @@ def hebb_update(w_, inp, outp, outpavg, lr):
 	dw = torch.pow(dw2 * 3.0, 3.0) # cube is essential
 	ltp = clamp(dw, 0.0, 1.0) # make ltp / ltd asymmetrc
 	ltd = clamp(dw, -1.0, 0.0) # to keep weights from going to zero
-	# lr = lr / math.pow(inp.size(0), 0.75)
-	dw = ltp*lr + ltd*lr*2.0
+	lr = lr / math.pow(inp.size(0), 0.35)
+	dw = ltp*lr + ltd*lr*1.5
 	# the cube nonlinearity seems to work better than straight hebbian, it pretty reliably converges.
 	if print_dw:
 		print('hebb')
@@ -41,7 +41,7 @@ def hebb_update(w_, inp, outp, outpavg, lr):
 	#scale = scale * scale2 # so if the output is too low, scale it up. 
 	dw = torch.outer(scale, torch.ones(inp.size(0)))
 	w_ = torch.mul(w_, dw)
-	# w_ = clamp(w_, -0.02, 1.0) # careful about this -- reverse weights need to be large
+	# w_ = clamp(w_, -0.02, 1.0) # careful about this -- reverse weights need to be large-2.0, 2.0
 	# clamping seems unnecessary if the rules are stable--? 
 	return w_
 
@@ -55,21 +55,28 @@ def inhib_update(w_, l, li, lr):
 	# l, the current layer e+i activation aka u. 
 	# li, layer inhibition (for down scaling)
 	# lr, learning rate. 
-	# lr = lr / math.pow(l.size(0), 0.75)
-	dw = torch.outer(l, l) # this doesn't break symmetry
-	dw2 = torch.outer(torch.pow(l, 2.0), torch.ones(l.size(0))) # this does!
-	# it also forces the diagonal to zero. 
-	dw2 = dw - dw2
-	dw2 = torch.clamp(dw2, -0.02, 0.2) # adjust this to change sparsity
-	dw2 = torch.mul(dw2, lr)
-	if print_dw:
+	lr = lr / math.pow(l.size(0), 0.35)
+	s = l.size(0)
+	a = outer(l, torch.ones(s))
+	b = outer(torch.ones(s), l + torch.randn(s) * 0.0)
+	dw = (b @ b) @ a - (a @ a) @ b
+	# dw = (a *b - a*a) *b, where a is pre and b is post.
+	# want to break symmetry and scale by post activity
+	dw = torch.clamp(dw, -0.05, 0.2) # adjust this to change sparsity
+	dw = torch.mul(dw, lr)
+	if False:
+		print('ls')
+		print(l)
+		print('li')
+		print(li)
 		print('inhib')
-		print(dw2)
-	w_ = torch.add(w_, dw2)
+		print(dw)
+		print(' ')
+	w_ = torch.add(w_, dw)
 	# checked: this is needed for sparsity. 
-	# surprisingly, adding weight down-scaling makes them *larger* - ? 
-	scale = torch.clamp(li, 2.5, 1e6)
-	scale = torch.sub(scale, 2.5)
+	# surprisingly, adding weight down-scaling makes them *larger* - ??
+	scale = torch.clamp(li, 2.0, 1e6)
+	scale = torch.sub(scale, 2.0)
 	scale = torch.exp(torch.mul(scale, -0.06))
 	one = torch.ones(w_.size(1))
 	dw = torch.outer(scale, one) 
@@ -82,26 +89,26 @@ def test_inihib_update():
 	im = [0,0,0]
 	cbar = [0,0,0]
 	for k in range(3): 
-		data = np.linspace(0.0, 2.0, 16)
-		data = np.reshape(data, (4, 4))
+		data = np.linspace(0.0, 2.5, 9)
+		data = np.reshape(data, (3, 3))
 		im[k] = axs[k].imshow(data, cmap = 'turbo')
 		cbar[k] = plt.colorbar(im[k], ax = axs[k])
-	wf = torch.rand(16, 2) / 3
-	wi = zeros(16,16)
+	wf = torch.rand(9, 3) / 1
+	wi = zeros(9,9)
 	for k in range(100000): 
-		#x = zeros(3)
-		#x[k%3] = 1.0
-		x = torch.rand(2)
+		x = zeros(3)
+		x[k%3] = 1.0
+		#x = torch.rand(3)
 		le = clamp(wf @ x, 0.0, 2.5)
-		li = clamp(wi @ le, 0.0, 2.5)
+		li = clamp(wi @ le, 0.0, 5.0)
 		ls = le - li
 		wi = inhib_update(wi, ls, li, 0.01)
 		if k % 101 == 100:
-			im[0].set_data(np.reshape(le, (4, 4)))
+			im[0].set_data(np.reshape(le, (3, 3)))
 			axs[0].set_title('le')
-			im[1].set_data(np.reshape(li, (4, 4)))
+			im[1].set_data(np.reshape(li, (3, 3)))
 			axs[1].set_title('li')
-			im[2].set_data(np.reshape(ls, (4, 4)))
+			im[2].set_data(np.reshape(ls, (3, 3)))
 			axs[2].set_title('ls')
 			fig.tight_layout()
 			fig.canvas.draw()
@@ -134,29 +141,28 @@ def make_stim(i):
 	return gauss_stim(cx, cy, 5.0)
 	
 
-batch_size = 1
-	
-train_loader = torch.utils.data.DataLoader(
-  torchvision.datasets.MNIST('files/', train=True, download=True, 
-                             transform=torchvision.transforms.Compose([
-                               torchvision.transforms.ToTensor()
-                             ])),
-  batch_size=1, shuffle=True, pin_memory=True)
-test_loader = torch.utils.data.DataLoader(
-  torchvision.datasets.MNIST('files/', train=False, download=True,
-                             transform=torchvision.transforms.Compose([
-                               torchvision.transforms.ToTensor()
-                             ])),
-  batch_size=batch_size, shuffle=True, pin_memory=True)
+#batch_size = 1
+#train_loader = torch.utils.data.DataLoader(
+  #torchvision.datasets.MNIST('files/', train=True, download=True, 
+                             #transform=torchvision.transforms.Compose([
+                               #torchvision.transforms.ToTensor()
+                             #])),
+  #batch_size=1, shuffle=True, pin_memory=True)
+#test_loader = torch.utils.data.DataLoader(
+  #torchvision.datasets.MNIST('files/', train=False, download=True,
+                             #transform=torchvision.transforms.Compose([
+                               #torchvision.transforms.ToTensor()
+                             #])),
+  #batch_size=batch_size, shuffle=True, pin_memory=True)
 HID1 = 128
 HID2 = 32
 
 # note: these are uniform random numbers, so they average to 0.5
 # unform would need the square root. 
-w_f1 = torch.mul(torch.rand(HID1, 28*28), 4.0 / (28*28))
+w_f1 = torch.zeros(HID1, 28*28)
 w_b1 = torch.zeros(28*28, HID1) # let hebb fill these in. 
 w_l2i = torch.zeros(HID1, HID1)
-w_f2 = torch.mul(torch.rand(HID2, HID1), 4.0 / (HID1))
+w_f2 = torch.zeros(HID2, HID1)
 w_b2 = torch.zeros(HID1, HID2) # let hebb fill these in. 
 w_l3i = torch.zeros(HID2, HID2)
 
@@ -173,27 +179,27 @@ if animate:
 initialized = False
 im = [ [0]*3 for i in range(5)]
 cbar = [ [0]*3 for i in range(5)]
-lr = 0.0002 # if the learning rate is too high, it goes chaoitc
+lr = 0.01 # if the learning rate is too high, it goes chaoitc
 for k in range(25): 
-	mnist = enumerate(train_loader)
+	#mnist = enumerate(train_loader)
 	for i in range(N): 
-		batch_idx, (indata, target) = next(mnist)
-		#indata = make_stim(i)
+		#batch_idx, (indata, target) = next(mnist)
+		indata = make_stim(i)
 		indata = torch.reshape(indata, (28*28,))
-
-		l1e = indata.to(torch.float)
+		# l1e = torch.cat((indata, -indata))
+		l1e = indata
 
 		l2e = clamp(w_f1 @ l1e, -0.5, 2.5) + torch.randn(HID1) * 0.025 
 		l2li = clamp(w_l2i @ l2e, 0.0, 5.0)
 		l2s = l2e - l2li # sparsified
 		l1i = clamp(w_b1 @ l2s, 0.0, 1.0) 
-		err = l1e - l1i
 		
-		# iterate .. ?  gradient-boosting? 
-		l2e = clamp(w_f1 @ (l1e + 0.5*err), -0.5, 2.5) + torch.randn(HID1) * 0.025 
-		l2li = clamp(w_l2i @ l2e, 0.0, 5.0)
-		l2s = l2e - l2li # sparsified
-		l1i = clamp(w_b1 @ l2s, 0.0, 1.0) 
+		## iterate .. ?  gradient-boosting? 
+		#err = l1e - l1i
+		#l2e = clamp(w_f1 @ (l1e + 0.5*err), -0.5, 2.5) + torch.randn(HID1) * 0.025 
+		#l2li = clamp(w_l2i @ l2e, 0.0, 5.0)
+		#l2s = l2e - l2li # sparsified
+		#l1i = clamp(w_b1 @ l2s, 0.0, 1.0) 
 									  
 		l3e = clamp(w_f2 @ l2e, 0.0, 2.5) + torch.randn(HID2) * 0.025 
 		l3li = clamp(w_l3i @ l3e, 0.0, 5.0)
@@ -211,38 +217,53 @@ for k in range(25):
 		w_b2 = hebb_update(w_b2, l3u, l2u, ones(HID1), lr)
 		w_b1 = hebb_update(w_b1, l2s, l1u, ones(28*28), lr)
 		
-		#w_l2i = inhib_update(w_l2i, l2s, l2li, lr*1.0)
-		#w_l3i = inhib_update(w_l3i, l3s, l3li, lr*1.0)
+		w_l2i = inhib_update(w_l2i, l2s, l2li, lr*1.0)
+		w_l3i = inhib_update(w_l3i, l3s, l3li, lr*1.0)
 		
 		# these are images; too complicated to write to stdout. 
 		def plot_tensor(r, c, v, name, lo, hi):
-			if v.shape[0] == 28*28:
-				v = torch.reshape(v, (28,28))
-			if v.shape[0] == 256:
-				v = torch.reshape(v, (16,16))
-			if v.shape[0] == 128:
-				v = torch.reshape(v, (8,16))
-			if v.shape[0] == 64:
-				v = torch.reshape(v, (8,8))
-			if v.shape[0] == 32:
-				v = torch.reshape(v, (8,4))
-			if v.shape[0] == 10:
-				v = torch.reshape(v, (2,5))
+			if len(v.shape) == 1:
+				if v.shape[0] == 2*28*28:
+					v = torch.reshape(v, (2,28,28))
+					v = torch.cat((v[0,:,:], v[1,:,:]), 1)
+				if v.shape[0] == 28*28:
+					v = torch.reshape(v, (28,28))
+				if v.shape[0] == 256:
+					v = torch.reshape(v, (16,16))
+				if v.shape[0] == 128:
+					v = torch.reshape(v, (8,16))
+				if v.shape[0] == 64:
+					v = torch.reshape(v, (8,8))
+				if v.shape[0] == 32:
+					v = torch.reshape(v, (4,8))
+				if v.shape[0] == 10:
+					v = torch.reshape(v, (2,5))
+			if len(v.shape) == 2:
+				if v.shape[1] == 28 * 28:
+					q = torch.reshape(v, (HID1, 28, 28))
+					# display a sample of the tuning functions
+					b = [torch.cat([q[i*12+j] for j in range(12)], 1) for i in range(6)]
+					v = torch.cat(b)
 			if not initialized:
 				# seed with random data so we get the range right
+				cmap_name = 'PuRd' # purple-red
+				if lo == -1*hi:
+					cmap_name = 'seismic'
 				data = np.random.rand(v.shape[0], v.shape[1]) * (hi-lo) + lo
-				im[r][c] = axs[r,c].imshow(data, cmap = 'turbo')
+				im[r][c] = axs[r,c].imshow(data, cmap = cmap_name)
 				cbar[r][c] = plt.colorbar(im[r][c], ax=axs[r,c])
 			im[r][c].set_data(v.numpy())
 			cbar[r][c].update_normal(im[r][c]) # probably does nothing
 			axs[r,c].set_title(name)
 			
-		if (i % 2500) >= 2491: 
+		if (i % 3500) >= 3491: 
 			if not animate:
 				fig, axs = plt.subplots(4, 3, figsize=(16, 9))
-			plot_tensor(0, 0, l1e, 'l1e', 0.0, 2.0)
-			plot_tensor(2, 0, l1i, 'l1i', 0.0, 2.0)
-			plot_tensor(3, 0, l1u, 'l1u', -1.0, 1.0)
+			plot_tensor(0, 0, l1e, 'l1e', -2.0, 2.0)
+			plot_tensor(1, 0, l1i, 'l1i', 0.0, 2.0)
+			plot_tensor(2, 0, l1u, 'l1u', -1.0, 1.0)
+			plot_tensor(3, 0, w_f1, 'w_f1', -0.2, 0.2)
+			plot_tensor(4, 0, w_b1.T, 'w_b1', -0.5, 0.5)
 			
 			plot_tensor(0, 1, l2e, 'l2e', 0.0, 2.5)
 			plot_tensor(1, 1, l2li, 'l2li', 0.0, 5.0)
@@ -252,8 +273,9 @@ for k in range(25):
 			
 			plot_tensor(0, 2, l3e, 'l3e', 0.0, 2.5)
 			plot_tensor(1, 2, l3li, 'l3li', 0.0, 2.5)
-			plot_tensor(3, 2, l3s, 'l3s', -2.5, 2.5)
-			plot_tensor(4, 2, l3u, 'l3u', -2.5, 2.5)
+			plot_tensor(2, 2, l3s, 'l3s', -2.5, 2.5)
+			plot_tensor(3, 2, l3u, 'l3u', -2.5, 2.5)
+			plot_tensor(4, 2, w_f2, 'w_f2', -2.0, 2.0)
 			
 			fig.tight_layout()
 			fig.canvas.draw()
