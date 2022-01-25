895082fb4dd253f72c190877873a5ace2ef3f436
Tim Hanson <sideskate@gmail.com>
2022-01-24 21:34:45 -0500

    it works on the bumps task!  cleanly!

--- cortex_mnist.py
+++ cortex_mnist.py
@@ -13,26 +13,28 @@ print_dw = False
 fixed_init = False 
 
 def hebb_update(w_, inp, outp, outpavg, lr):
-	dw = torch.outer(outp, inp) 
+	#dw = torch.outer(torch.pow(outp, 3), inp) # move the nonlinearity to the output
+	dw = torch.outer(outp, inp)
 	# note: dw needs to be both positive and negative.  
 	# I tried clamping to [0 1] so as to feed into a power nonlinearity, 
 	# but this resulted in the weights all coverging to ~1. 
 	# negative hebbian updates are necessary. 
 	dw2 = clamp(dw, -1.0, 1.0)
 	dw = torch.pow(dw2 * 3.0, 3.0) # cube is essential
-	ltp = clamp(dw, 0.0, 1.0) # make ltp / ltd asymmetrc
-	ltd = clamp(dw, -1.0, 0.0) # to keep weights from going to zero
+	#dw = dw2
+	ltp = clamp(dw, 0.0, 2.0) # make ltp / ltd asymmetrc
+	ltd = clamp(dw, -2.0, 0.0) # to keep weights from going to zero
 	lr = lr / math.pow(inp.size(0), 0.35)
-	dw = ltp*lr + ltd*lr*1.5
+	dw = ltp*lr + ltd*lr*3.0
 	# the cube nonlinearity seems to work better than straight hebbian, it pretty reliably converges.
 	if print_dw:
 		print('hebb')
 		print(dw)
 	w_ = torch.add(w_, dw)
 	# also perform scaling. 
-	scale = torch.clamp(outp, 1.3, 1e6)
-	scale = torch.sub(scale, 1.3)
-	scale = torch.exp(torch.mul(scale, -0.08)) 
+	scale = torch.clamp(outpavg, 0.75, 1e6)
+	scale = torch.sub(scale, 0.75)
+	scale = torch.exp(torch.mul(scale, -0.06)) 
 	# scale2 makes the learning unstable! 
 	# need another means of allocation and de-allocation of units... 
 	#scale2 = torch.clamp(outp, 0.0, 0.1) 
@@ -41,7 +43,7 @@ def hebb_update(w_, inp, outp, outpavg, lr):
 	#scale = scale * scale2 # so if the output is too low, scale it up. 
 	dw = torch.outer(scale, torch.ones(inp.size(0)))
 	w_ = torch.mul(w_, dw)
-	# w_ = clamp(w_, -0.02, 1.0) # careful about this -- reverse weights need to be large-2.0, 2.0
+	w_ = clamp(w_, -0.0, 1.0) # careful about this -- reverse weights need to be large-2.0, 2.0
 	# clamping seems unnecessary if the rules are stable--? 
 	return w_
 
@@ -160,6 +162,7 @@ HID2 = 32
 # note: these are uniform random numbers, so they average to 0.5
 # unform would need the square root. 
 w_f1 = torch.zeros(HID1, 28*28)
+#w_f1 = torch.rand(HID1, 28*28) / 28
 w_b1 = torch.zeros(28*28, HID1) # let hebb fill these in. 
 w_l2i = torch.zeros(HID1, HID1)
 w_f2 = torch.zeros(HID2, HID1)
@@ -179,7 +182,7 @@ if animate:
 initialized = False
 im = [ [0]*3 for i in range(5)]
 cbar = [ [0]*3 for i in range(5)]
-lr = 0.01 # if the learning rate is too high, it goes chaoitc
+lr = 0.005 # if the learning rate is too high, it goes chaoitc
 for k in range(25): 
 	#mnist = enumerate(train_loader)
 	for i in range(N): 
@@ -189,19 +192,19 @@ for k in range(25):
 		# l1e = torch.cat((indata, -indata))
 		l1e = indata
 
-		l2e = clamp(w_f1 @ l1e, -0.5, 2.5) + torch.randn(HID1) * 0.025 
+		l2e = clamp(w_f1 @ l1e, -0.5, 1.5) + torch.randn(HID1) * 0.1 
+		l2li = clamp(w_l2i @ l2e, 0.0, 5.0)
+		l2s = l2e - l2li # sparsified
+		l1i = clamp(w_b1 @ l2s, 0.0, 1.5) 
+		
+		# iterate .. ?  gradient-boosting? 
+		err = l1e - l1i
+		l2e = clamp(w_f1 @ (l1e + 0.5*err), -0.5, 2.5) + torch.randn(HID1) * 0.1 
 		l2li = clamp(w_l2i @ l2e, 0.0, 5.0)
 		l2s = l2e - l2li # sparsified
 		l1i = clamp(w_b1 @ l2s, 0.0, 1.0) 
-		
-		## iterate .. ?  gradient-boosting? 
-		#err = l1e - l1i
-		#l2e = clamp(w_f1 @ (l1e + 0.5*err), -0.5, 2.5) + torch.randn(HID1) * 0.025 
-		#l2li = clamp(w_l2i @ l2e, 0.0, 5.0)
-		#l2s = l2e - l2li # sparsified
-		#l1i = clamp(w_b1 @ l2s, 0.0, 1.0) 
 									  
-		l3e = clamp(w_f2 @ l2e, 0.0, 2.5) + torch.randn(HID2) * 0.025 
+		l3e = clamp(w_f2 @ l2e, 0.0, 2.5) + torch.randn(HID2) * 0.1 
 		l3li = clamp(w_l3i @ l3e, 0.0, 5.0)
 		l3s = l3e - l3li
 		l2i = clamp(w_b2 @ l3s, 0.0, 5.0)
@@ -214,11 +217,11 @@ for k in range(25):
 		
 		w_f1 = hebb_update(w_f1, l1u, l2s, l2a, lr)
 		w_f2 = hebb_update(w_f2, l2u, l3s, l3a, lr)
-		w_b2 = hebb_update(w_b2, l3u, l2u, ones(HID1), lr)
-		w_b1 = hebb_update(w_b1, l2s, l1u, ones(28*28), lr)
+		w_b2 = hebb_update(w_b2, l3u, l2u, 0.5*ones(HID1), lr)
+		w_b1 = hebb_update(w_b1, l2s, l1u, 0.5*ones(28*28), lr)
 		
-		w_l2i = inhib_update(w_l2i, l2s, l2li, lr*1.0)
-		w_l3i = inhib_update(w_l3i, l3s, l3li, lr*1.0)
+		#w_l2i = inhib_update(w_l2i, l2s, l2li, lr*1.0)
+		#w_l3i = inhib_update(w_l3i, l3s, l3li, lr*1.0)
 		
 		# these are images; too complicated to write to stdout. 
 		def plot_tensor(r, c, v, name, lo, hi):
@@ -260,7 +263,7 @@ for k in range(25):
 			if not animate:
 				fig, axs = plt.subplots(4, 3, figsize=(16, 9))
 			plot_tensor(0, 0, l1e, 'l1e', -2.0, 2.0)
-			plot_tensor(1, 0, l1i, 'l1i', 0.0, 2.0)
+			plot_tensor(1, 0, l1i, 'l1i', -2.0, 2.0)
 			plot_tensor(2, 0, l1u, 'l1u', -1.0, 1.0)
 			plot_tensor(3, 0, w_f1, 'w_f1', -0.2, 0.2)
 			plot_tensor(4, 0, w_b1.T, 'w_b1', -0.5, 0.5)
